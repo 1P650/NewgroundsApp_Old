00c1fee449b48805b741dbc7cb7a327d5296567e
jewpigeon <jewpigeon@gmail.com>
2020-06-29 16:31:47 +0300

    Che-che-che-che-changes

--- app/src/main/java/jewpigeon/apps/newgrounds/ContentFragments/FeaturedPortal.java
+++ app/src/main/java/jewpigeon/apps/newgrounds/ContentFragments/FeaturedPortal.java
@@ -1,20 +1,17 @@
 package jewpigeon.apps.newgrounds.ContentFragments;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import jewpigeon.apps.newgrounds.Fundamental.NG_Fragment;
 import jewpigeon.apps.newgrounds.R;
 
+
 public class FeaturedPortal extends NG_Fragment {
-    CoordinatorLayout.Behavior behavior;
     View rootView;
     public static FeaturedPortal newInstance() {
         
@@ -35,39 +32,9 @@ public class FeaturedPortal extends NG_Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.content_featured, container, false);
+        setSeekerView(rootView);
         return rootView;
     }
 
 
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-
-        if(behavior != null)
-            return;
-
-        FrameLayout layout =(FrameLayout) getNGActivity().findViewById(R.id.content_container);
-        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) layout.getLayoutParams();
-
-        behavior = params.getBehavior();
-        params.setBehavior(null);
-
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        if(behavior == null)
-            return;
-
-        FrameLayout layout =(FrameLayout) getNGActivity().findViewById(R.id.content_container);
-        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) layout.getLayoutParams();
-
-        params.setBehavior(behavior);
-
-        layout.setLayoutParams(params);
-
-        behavior = null;
-    }
 }
